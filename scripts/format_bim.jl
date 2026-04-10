7e1d732f347de4a062033a78968c96bcba99ebeb
Olivier Labayle <olabayle@gmail.com>
2026-04-10 13:08:14 +0100

    update format bim

--- scripts/format_bim.jl
+++ scripts/format_bim.jl
@@ -5,8 +5,10 @@ function main()
     bim_file = ARGS[1]
     bim = CSV.read(bim_file, DataFrame; header=[:CHROM, :ID, :CM, :POS, :A1, :A2])
     transform!(
-        bim, 
-        [:CHROM, :POS, :A1, :A2] => ByRow((c, p, a1, a2) -> string(c, ":", p, ":", join(sort([a1, a2]), ":"))) => :ID
+        bim,
+        :A1 => (x -> replace.(x, "-" => "0")) => :A1,
+        :A2 => (x -> replace.(x, "-" => "0")) => :A2,
+        [:CHROM, :POS] => ByRow((c, p, a1, a2) -> string(c, ":", p)) => :ID
     )
     CSV.write(bim_file, bim; header=false, delim="\t")
     return 0
