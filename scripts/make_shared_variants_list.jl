55fea50455ce2532439bf32c54f1c42000468e31
Olivier Labayle <olabayle@gmail.com>
2026-04-10 13:15:11 +0100

    fix shared variants

--- scripts/make_shared_variants_list.jl
+++ scripts/make_shared_variants_list.jl
@@ -1,19 +1,18 @@
 using CSV
 using DataFrames
 
-function get_bim_positions(prefix)
+function get_bim_ids(prefix)
     bim = CSV.read(string(prefix, ".bim"), DataFrame; header=[:CHROM, :ID, :CM, :POS, :A1, :A2])
     return bim.ID
 end
 
 function main()
-    copy!(ARGS, ["merge_list.txt", "shared_variants.txt"])
     # Find shared variants
     prefixes = readlines(ARGS[1])
     first_prefix = pop!(prefixes)
-    shared_ids = get_bim_positions(first_prefix)
+    shared_ids = get_bim_ids(first_prefix)
     for prefix in prefixes
-        bim_ids = get_bim_positions(prefix)
+        bim_ids = get_bim_ids(prefix)
         intersect!(shared_ids, bim_ids)
     end
     # Write them to file
